744d7c675899ee013c6ef88c5acd9f5893c03f9b
Damien Altenburg <daltenburg@rrc.ca>
2024-10-05 14:07:01 -0500

    Update script to include path to the script.

--- working_with_files.py
+++ working_with_files.py
@@ -4,5 +4,16 @@ Author: Damien Altenburg
 Date: 2023-10-1
 Usage: python working_with_files.py
 """
-file_path = "data.txt"
-file = None
+
+from os import path
+
+script_directory = path.dirname(path.realpath(__file__))
+filename = "data.txt"
+file_path = f"{script_directory}/{filename}"
+data_file = None
+
+with open(file_path) as data_file:
+    for record in data_file:
+        print(record)
+
+input("Press <enter> to continue...")
