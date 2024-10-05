8ec1d3b9199807d692165a7d5e4aa7956b29a94a
Damien Altenburg <daltenburg@rrc.ca>
2024-10-05 14:14:00 -0500

    Update script to include a completed program.

--- guessing_game.py
+++ guessing_game.py
@@ -1,16 +1,52 @@
 """
-Description: Demonstrates how to handle exceptions.
+Description: A simple guessing game.
 Author: Damien Altenburg
 Date: 2023-10-1
 Usage: python guessing_game.py
 """
+
 from random import randint
 
-LOW_RANGE = 1
-HIGH_RANGE = 10
+title_text: str = "** JELLYBEAN GUESSING GAME **"
 
-NUMBER_OF_JELLYBEANS = randint(LOW_RANGE, HIGH_RANGE)
+LOW_RANGE: int = 1
+HIGH_RANGE: int = 10
+NUMBER_OF_JELLYBEANS: int = randint(LOW_RANGE, HIGH_RANGE)
 
-guess = 0
-guess_count = 0
-valid_guess_values = range(LOW_RANGE, HIGH_RANGE + 1)
+guess_count: int = 0
+
+print(title_text)
+print("=" * len(title_text))
+
+prompt_text = "Enter your guess: "
+
+jellybeans_guessed = False
+
+while not jellybeans_guessed:
+    guess_is_out_of_range = True
+
+    if guess_count > 0:
+        print("That guess is incorrect.")
+
+        prompt_text = "Guess again: "
+
+    while guess_is_out_of_range:
+        try:
+            guess = input(prompt_text)
+            guess = int(guess)
+
+            guess_is_out_of_range = guess < LOW_RANGE or guess > HIGH_RANGE
+
+            if guess_is_out_of_range:
+                print(f"Your guess must be between {LOW_RANGE}-{HIGH_RANGE}.")
+                prompt_text = f"Guess ({LOW_RANGE}-{HIGH_RANGE}): "
+        except ValueError as exception:
+            print("You must enter a whole number.")
+            
+            prompt_text = "Re-enter guess: "
+
+    guess_count += 1
+
+    jellybeans_guessed = guess == NUMBER_OF_JELLYBEANS
+
+print(f"It took you {guess_count} guess{'' if guess_count == 1 else 'es'}.")
